f7c70626ebf67c852c02fb08ff50d62055efb18a
BlackPhlox <theluja@gmail.com>
2021-08-07 01:18:51 +0200

    Init

---
+++ README.md
@@ -0,0 +1,52 @@
+# Bevy Camera Gizmo
+
+This Bevy plugin adds a transform gizmo to entities in the scene, allowing you to drag and rotate meshes with your mouse.
+
+# Demo
+
+Run a minimal implementation of the gizmo by cloning this repository and running:
+
+```shell
+cargo run --example minimal
+```
+
+# Features
+
+* Prebuilt transform gizmo appears when you select a designated mesh
+* Translation handles
+* Rotation handles
+* Gizmo always renders on top of the main render pass
+* Gizmo scales at it moves closer/further from the camera
+
+# Usage
+
+This plugin is built on and relies on [`bevy_mod_picking`](https://github.com/aevyrie/bevy_mod_picking) for 3d mouse interaction with the scene.
+
+Add the plugin to the `[dependencies]` in `Cargo.toml`
+
+```toml
+bevy_cam_gizmo = { git = "https://github.com/BlackPhlox/bevy_cam_gizmo", branch = "main" }
+```
+
+You will need to add the transform gizmo plugin, as well as make sure you have also brought in the picking plugin.
+
+```rust
+.add_plugin(bevy_mod_picking::DefaultPickingPlugins)
+.add_plugin(bevy_cam_gizmo::TransformGizmoPlugin)
+```
+
+Next, you will need to mark your picking camera as your gizmo camera:
+
+```rust
+.insert_bundle(bevy_mod_picking::PickingCameraBundle::default())
+.insert(bevy_cam_gizmo::GizmoPickSource::default());
+```
+
+Finally, mark any meshes you want to be transformed with the gizmo; note they must also be selectable in the picking plugin:
+
+```rust
+.insert_bundle(bevy_mod_picking::PickableBundle::default())
+.insert(bevy_transform_gizmo::GizmoTransformable);
+```
+
+See the [minimal](examples/minimal.rs) demo for an example of a minimal implementation.
---
+++ src/picking.rs
@@ -0,0 +1,77 @@
+use bevy::prelude::*;
+
+use crate::TransformGizmo;
+
+pub type GizmoPickSource = bevy_mod_raycast::RayCastSource<GizmoRaycastSet>;
+pub type PickableGizmo = bevy_mod_raycast::RayCastMesh<GizmoRaycastSet>;
+
+/// Plugin with all the systems and resources used to raycast against gizmo handles separately from
+/// the `bevy_mod_picking` plugin.
+pub struct GizmoPickingPlugin;
+impl Plugin for GizmoPickingPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<bevy_mod_raycast::PluginState<GizmoRaycastSet>>()
+            .add_system_to_stage(
+                CoreStage::PreUpdate,
+                bevy_mod_raycast::build_rays::<GizmoRaycastSet>
+                    .system()
+                    .label(bevy_mod_raycast::RaycastSystem::BuildRays),
+            )
+            .add_system_to_stage(
+                CoreStage::PreUpdate,
+                bevy_mod_raycast::update_raycast::<GizmoRaycastSet>
+                    .system()
+                    .label(bevy_mod_raycast::RaycastSystem::UpdateRaycast)
+                    .after(bevy_mod_raycast::RaycastSystem::BuildRays),
+            )
+            .add_system_to_stage(
+                CoreStage::PreUpdate,
+                update_gizmo_raycast_with_cursor
+                    .system()
+                    .before(bevy_mod_raycast::RaycastSystem::BuildRays),
+            )
+            .add_system_to_stage(
+                CoreStage::PreUpdate,
+                disable_mesh_picking_during_gizmo_hover
+                    .system()
+                    .before(bevy_mod_picking::PickingSystem::Focus)
+                    .after(bevy_mod_raycast::RaycastSystem::UpdateRaycast),
+            );
+    }
+}
+
+pub struct GizmoRaycastSet;
+
+/// Update the gizmo's raycasting source with the current mouse position.
+fn update_gizmo_raycast_with_cursor(
+    mut cursor: EventReader<CursorMoved>,
+    mut query: Query<&mut bevy_mod_raycast::RayCastSource<GizmoRaycastSet>>,
+) {
+    for mut pick_source in &mut query.iter_mut() {
+        // Grab the most recent cursor event if it exists:
+        if let Some(cursor_latest) = cursor.iter().last() {
+            pick_source.cast_method =
+                bevy_mod_raycast::RayCastMethod::Screenspace(cursor_latest.position);
+        }
+    }
+}
+
+/// Disable the picking plugin when the mouse is over one of the gizmo handles.
+fn disable_mesh_picking_during_gizmo_hover(
+    mut picking_state: ResMut<bevy_mod_picking::PickingPluginState>,
+    query: Query<&bevy_mod_raycast::RayCastSource<GizmoRaycastSet>>,
+    gizmo_query: Query<&TransformGizmo>,
+) {
+    let not_hovering_gizmo = if let Some(source) = query.iter().last() {
+        source.intersect_top().is_none()
+    } else {
+        true
+    };
+    let gizmo_inactive = if let Some(gizmo) = gizmo_query.iter().last() {
+        gizmo.current_interaction().is_none()
+    } else {
+        return;
+    };
+    // Set the picking state based on current user interaction state
+    picking_state.enabled = gizmo_inactive && not_hovering_gizmo;
+}
