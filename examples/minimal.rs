a4cd377d4c0da94b51da2b941461f97db5279fec
Mikkel Rasmussen <theluja@gmail.com>
2022-08-30 16:22:42 +0200

    Fixed cone

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -2,11 +2,6 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .insert_resource(WindowDescriptor {
-            vsync: false, // Disabled for this demo to remove vsync as a source of input latency
-            ..Default::default()
-        })
-        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .run();
 }
